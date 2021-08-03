00283fe8f8330b3a9e5baad9e91b186870dd6446
Aadil Jamal <aadil.soud@student.moringaschool.com>
2021-08-03 09:55:31 +0300

    added form to html , added the validate function to check correct entry of values

--- js/script.js
+++ js/script.js
@@ -2,3 +2,26 @@ var CC, YY, MM, DD, d, dayValue;
 var dayNames = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
 var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
 var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
+
+function validate(){
+  var genders = document.getElementsByName("gender");
+  if( document.form.year.value == "" || document.form.year.value.length !=4 || document.form.year.value >2100 || document.form.year.value <=1900) {
+     alert( "Please provide a valid year of birth! eg 2019" );
+     return false;
+  }
+  else if( document.form.month.value == "" || document.form.month.value.length != 2 || document.form.month.value > 12  || document.form.month.value <= 0){
+     alert( "Please provide your month of birth! between 1 and 12" );
+     return false;
+  }
+  else if( document.form.date.value == "" || document.form.month.value.length != 2|| document.form.date.value > 31 || document.form.date.value <= 0) {
+     alert( "Please provide a valid date that you were born in!" );
+     return false;
+  }
+  else if(genders[0].checked==false && genders[1].checked==false ) {
+      alert("You must select male or female");
+      return false;
+  }   
+  else{
+    return true ;
+  }
+}
